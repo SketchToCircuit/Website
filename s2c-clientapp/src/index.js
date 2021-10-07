78622de6fb23470760a62f658d95926da92cc114
SaltySebi <53235949+SaltySebi@users.noreply.github.com>
2021-10-07 10:51:49 +0200

    Basisches Topnav

--- s2c-clientapp/src/index.js
+++ s2c-clientapp/src/index.js
@@ -3,50 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Mainpage from './Mainpage'
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 const App = () => {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/about">
-            <h1>About</h1>
-          </Route>
-          <Route path="/users">
-            <h1>Users</h1>
-          </Route>
-          <Route path="/">
-            <Mainpage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div class="topnav">
+                <a class="HomeLink" href="/">SketchtoCircuit</a>
+                <a href="/Login">Login</a>
+                <a href="/About">About</a>
+            </div>
+            
+            <Switch>
+                <Route path="/about">
+                    <h1>About</h1>
+                </Route>
+                <Route path="/users">
+                    <h1>Users</h1>
+                </Route>
+                <Route path="/">
+                    <Mainpage/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+    <App/>
+</React.StrictMode>, document.getElementById('root'));
