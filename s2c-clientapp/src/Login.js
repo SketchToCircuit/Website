709e953d7920711d1a9cb45cf50f3e3077423101
PatrickZ1 <36849297+PatrickZ1@users.noreply.github.com>
2021-10-08 08:49:04 +0200

    working Google OAuth clientID

--- s2c-clientapp/src/Login.js
+++ s2c-clientapp/src/Login.js
@@ -5,11 +5,13 @@ const responseGoogle = (response) => {
     console.log(response);
 }
 
+const clientId = '406203004756-j13sirg99go7roat5eqmecn5ue8ahld9.apps.googleusercontent.com';
+
 class Login extends React.Component {
     render() {
         return (
         <GoogleLogin
-            clientId="197355798433-msk8hu5u74nlqsba1gf533flb3dkatgv.apps.googleusercontent.com"
+            clientId={clientId}
             render={renderProps => (
             <button className='login-button' onClick={renderProps.onClick} disabled={renderProps.disabled}>Login via Google</button>
             )}
