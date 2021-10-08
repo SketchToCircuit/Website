295d228c7966c23d8f033f76d2c222119b0327a6
PatrickZ1 <36849297+PatrickZ1@users.noreply.github.com>
2021-10-08 09:48:17 +0200

    initial websocket implementation

--- s2c-clientapp/src/index.js
+++ s2c-clientapp/src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import Mainpage from './Mainpage'
-import Login from './Login'
 import About from './About'
 import NotFound from './NotFound'
 
@@ -16,13 +15,11 @@ const App = () => {
             <Router>
                 <nav className="topnav">
                     <NavLink activeStyle={{textShadow: "0px 0px 2px" }} exact to="/" className='home-link'>SketchtoCircuit</NavLink>
-                    <NavLink activeStyle={{textShadow: "0px 0px 2px" }} to="/Login">Login</NavLink>
                     <NavLink activeStyle={{textShadow: "0px 0px 2px" }} to="/About">About</NavLink>
                 </nav>
 
                 <Switch>
                     <Route exact path="/" component={Mainpage}/>
-                    <Route path="/Login" component={Login}/>
                     <Route path="/About" component={About}/>
                     <Route component={NotFound}/>
                 </Switch>
