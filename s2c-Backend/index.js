26b5801ccd332f13efd818fb587ba575107fd5e1
PatrickZ1 <36849297+PatrickZ1@users.noreply.github.com>
2021-10-11 13:39:57 +0200

    websocket message handling

--- s2c-Backend/index.js
+++ s2c-Backend/index.js
@@ -12,14 +12,19 @@ const HttpsServer = require('https');
     console.log(`Connection from ${req.socket.remoteAddress} with id ${count}`);
     clients.set(ws, count);
     count = count + 1;
-    ws.send('something');
+
+    // testing
+    const data = {'data' : {'type' : 'VALIDATION', 'imgSrc' : 'https://punkt.de/_Resources/Persistent/7/5/b/d/75bd14d2ed44da6bf49616fd635a36fa4bb2eac8/React_Native_Logo-1196x628.png'}}
+    sendData(data, ws);
+
     ws.on('message', function incoming(data) {
       receiveData(data, ws)
     });
+    
     ws.on('close', () => {
-      clients.delete(ws);
+        clients.delete(ws);
     })
-  });
+});
 
 function receiveData(data, ws)
 {
--- s2c-clientapp/src/Validation.js
+++ s2c-clientapp/src/Validation.js
@@ -2,25 +2,43 @@ import React from 'react';
 import './Validation.css';
 
 class Validation extends React.Component {
-    // Access websocket here with "this.props.ws"
-    // or if you need it more often: "const {ws} = this.props" und dann mit "ws."
+    // Access websocket here with "this.props.ws" or if you need it more often:
+    // "const {ws} = this.props" und dann mit "ws."
 
     constructor(props) {
         super(props)
 
         this.state = {
-            img_blob: ''    // BLOB Url of image; change state only with setState
+            imgSrc: '' // BLOB Url of image; change state only with setState
         };
+    }
+
+    componentDidMount() {
+        this.updateImgSrc();
+    }
+
+    componentDidUpdate() {
+        this.updateImgSrc();
+    }
+
+    updateImgSrc = () => {
+        const data = this.props.wsData;
+
+        if (data && data.imgSrc) {
+            if (this.state.imgSrc !== data.imgSrc) {
+                this.setState({imgSrc: data.imgSrc});
+            }
+        }
     }
 
     render() {
         return (
             <div className='validation'>
                 <h1>Validation</h1>
-                <img src={this.state.img_blob} alt=''></img>
+                <img src={this.state.imgSrc} alt=''></img>
             </div>
         );
     }
-  }
+}
 
 export default Validation;
