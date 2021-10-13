437db6aa53621331e22ab8d6717c70b5f6e1aedf
SaltySebi <53235949+SaltySebi@users.noreply.github.com>
2021-10-13 16:29:32 +0200

    implemented two stage canvas

--- s2c-clientapp/src/Components/Draw.jsx
+++ s2c-clientapp/src/Components/Draw.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import CanvasDraw from "react-canvas-draw";
 
 import '../Styles/Draw.css';
-
 class Draw extends React.Component {
     // Access websocket here with "this.props.ws" or if you need it more often:
     // "const {ws} = this.props" und dann mit "ws."
@@ -11,42 +10,56 @@ class Draw extends React.Component {
         super(props)
 
         this.state = {
-            img_blob: '', // BLOB Url of image; change state only with setState
+            isfirstDrawn: false,
+            procedebtntext: "Next",
+            backgroundpic: "logo512.png",
+            canvdimension: window.innerHeight * 0.8
         };
     }
 
+    prepareNext = () =>{
+        var buffer = this.saveableCanvas.canvas.drawing.toDataURL("image/png");
+        this.saveableCanvas.clear();
+        //pfush because resize is neede (aba a guada Pfush)
+        this.setState((state) => ({
+            backgroundpic: buffer,
+            procedebtntext: "Finish",
+            canvdimension:  this.state.canvdimension + 1 }));
+        this.setState((state) => ({
+            canvdimension:  this.state.canvdimension - 1 }));
+    }
+
     render() {
-        var pic = localStorage.getItem('firstDrawing')
-        console.log();
-        return (
-            <div>
-            <div className='draw'>
-                <CanvasDraw
-                    className="canvas-one"
-                    ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
-                    brushColor="#000000"
-                    brushRadius="2"
-                    lazyRadius="0"
-                    //min is 300px by 300px even older 4:3 screens can resolve this(i hope)
-                    canvasWidth={Math.max(300, window.innerHeight * 0.8)}
-                    canvasHeight={Math.max(300, window.innerHeight * 0.8)}
-                    //bs if somebody looks at it
-                    imgSrc={localStorage.getItem('firstDrawing')}
-                    />
-            </div>
-                <button className="next-btn"
-                onClick={() => {
-                    localStorage.setItem("firstDrawing", this.saveableCanvas.getSaveData());
-                }}>
-                    Save
-                </button>
-                <button className="undo-btn"
-                    onClick={() => {this.saveableCanvas.undo();
-                }}>
-                    Undo
-                </button>
-            </div>
-        );
+            return (
+                <div>
+                 <div className="draw-btn">
+                    <button className="next-btn"
+                    onClick={() => {
+                        this.prepareNext();
+                    }}>
+                        {this.state.procedebtntext}
+                    </button>
+                    <button className="undo-btn"
+                        onClick={() => {this.saveableCanvas.undo();
+                    }}>
+                        Undo
+                    </button>
+                    </div>
+                    <CanvasDraw
+                        id="mycanvas"
+                        className="canvas-one"
+                        ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
+                        brushColor="#000000"
+                        brushRadius={2}
+                        lazyRadius={0}
+                        //min is 300px by 300px even older 4:3 screens can resolve this(i hope)
+                        canvasWidth={this.state.canvdimension}
+                        canvasHeight={this.state.canvdimension}
+                        //bs if somebody looks at it
+                        imgSrc={this.state.backgroundpic}
+                        />
+                </div>
+            );    
     }
 }
 
