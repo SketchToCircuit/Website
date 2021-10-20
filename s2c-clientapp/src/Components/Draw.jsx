37b2833aedcdbc68de60a57b35a049f2a5c210f6
SaltySebi <53235949+SaltySebi@users.noreply.github.com>
2021-10-20 13:29:33 +0200

    Draw hint Com

--- s2c-clientapp/src/Components/Draw.jsx
+++ s2c-clientapp/src/Components/Draw.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import CanvasDraw from "react-canvas-draw";
 
 import '../Styles/Draw.css';
+
+var componentimage;
 class Draw extends React.Component {
     // Access websocket here with "this.props.ws" or if you need it more often:
     // "const {ws} = this.props" und dann mit "ws."
@@ -10,36 +12,82 @@ class Draw extends React.Component {
         super(props)
 
         this.state = {
+            hintpicture: "",
+            hinttext: "",
             isfirstDrawn: false,
             procedebtntext: "Next",
             backgroundpic: "",
-            canvdimension: window.innerHeight * 0.8
+            canvdimension: window.innerHeight * 0.8,
+            batchcount: 0,
+            type: ""
         };
     }
 
-    onButtonNext = () => {
-        console.log(this.saveableCanvas.canvas.interface);
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.wsData !== this.props.wsData) {
+            //backend isnt aloowed to send the same task twice
+            this.setState({hintpicture: this.props.wsData.ComponentHint.img, 
+                           hinttext: this.props.wsData.ComponentHint.text,
+                           type: this.props.type });
+        }
+    }
 
-        var buffer = this
-            .saveableCanvas
-            .canvas
-            .drawing
-            .toDataURL("image/png");
-        this
-            .saveableCanvas
-            .clear();
-        //pfush because resize is neede (aba a guada Pfush)
-        this.setState((state) => ({
-            backgroundpic: buffer,
-            procedebtntext: "Finish",
-            canvdimension: this.state.canvdimension + 1
-        }));
-        this.setState((state) => ({
-            canvdimension: this.state.canvdimension - 1
-        }));
+    onButtonNext = () => {
+        if (!this.state.isfirstDrawn) {
+            componentimage = this.saveableCanvas.canvas.drawing.toDataURL("image/png");
+            this.saveableCanvas.clear();
+            // pfush because resize is needed to update canvas since it doesnt update (aba aguada Pfush)
+            this.setState((state) => ({
+                backgroundpic: componentimage,
+                procedebtntext: "Finish",
+                isfirstDrawn: true,
+                hinttext: this.props.wsData.LabelHint.text,
+                hintpicture: this.props.wsData.LabelHint.img,
+                canvdimension: this.state.canvdimension + 1
+            }));
+            this.setState((state) => ({
+                canvdimension: this.state.canvdimension - 1
+            }));
+
+        } else if (this.state.isfirstDrawn) {
+
+            this.setState((state) => ({
+                backgroundpic: "",
+                procedebtntext: "Next",
+                isfirstDrawn: false,
+                canvdimension: this.state.canvdimension + 1
+            }));
+
+            const data = {
+                "PacketId": 104,
+                "Data": {
+                    "count": this.state.batchcount,
+                    "type": this.state.type,
+                    "compontentImg": componentimage,
+                    "labelImg": this.saveableCanvas.canvas.drawing.toDataURL("image/png")
+                }
+            }
+
+            this.saveableCanvas.clear();
+
+            try {
+                this.props.ws.send(JSON.stringify(data));
+            } catch (error) {
+                return;
+            }
+
+            if(this.state.batchcount == 5)
+            {
+                //callback hier um page zu wechseln
+                return;
+            }
+        }
     }
 
     render() {
+        if (!this.props.wsData) {
+            return null;
+        }
         return (
             <div className="wraping-div">
                 <div className="draw-btn">
@@ -61,20 +109,18 @@ class Draw extends React.Component {
                     </button>
                 </div>
 
-                <div className="canvas" >
-                    <CanvasDraw
-                      ref={canvasDraw => (this.saveableCanvas = canvasDraw)} 
-                      brushColor="#000000" 
-                      brushRadius={2} 
-                      lazyRadius={0} //min is 300px by 300px even older 4:3 screens can resolve this(i hope)
-                      canvasWidth={this.state.canvdimension} 
-                      canvasHeight={this.state.canvdimension} //bs if somebody looks at it
-                      imgSrc={this.state.backgroundpic}/>
+                <div className="canvas">
+                    <CanvasDraw ref={canvasDraw => (this.saveableCanvas = canvasDraw)} brushColor="#000000" brushRadius={2} lazyRadius={0} //min is 300px by 300px even older 4:3 screens can resolve this(i hope)
+                        canvasWidth={this.state.canvdimension} canvasHeight={this.state.canvdimension} //bs if somebody looks at it
+                        imgSrc={this.state.backgroundpic}/>
                 </div>
 
                 <div className="hint-div">
-                    <p className="instruction-paragraph">Please Draw this</p>
-                    <img src="logo512.png" className="hint-picture" width={this.state.canvdimension*0.8}/>
+                    <p className="instruction-paragraph">Please Draw this {this.state.hinttext}</p>
+                    <img
+                        src={this.state.hintpicture}
+                        className="hint-picture"
+                        width={this.state.canvdimension * 0.8}/>
                 </div>
 
             </div>
