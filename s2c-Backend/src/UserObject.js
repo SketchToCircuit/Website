32861979d2b214913c73a3eaf97ba5b6880ccdde
PatrickZ1 <36849297+PatrickZ1@users.noreply.github.com>
2021-10-28 18:36:40 +0200

    prefer types with fewer drawn images

--- s2c-Backend/src/UserObject.js
+++ s2c-Backend/src/UserObject.js
@@ -5,6 +5,7 @@ class UserObject {
         this.google = undefined; //Google token this.
         this.drawVal = undefined; //val or draw or non
         this.count = 0; // How many images have been drawn/validated?
+        this.lastDrawId = -1;   // last component_id the user did draw
     }
 }
 
--- s2c-Backend/src/mysql.js
+++ s2c-Backend/src/mysql.js
@@ -91,15 +91,17 @@ function getValidationData(base64Helper, googleId, callback) {
     });
 }
 
-function getDrawData(base64Helper,callback) {
-    let query = "SELECT * FROM component_types ORDER BY RAND() LIMIT 1;";
-    database.query(query, (err, result) => {
+function getDrawData(lastDrawId, base64Helper, callback) {
+    // prefer types with fewer drawn images
+    let query = "SELECT * FROM component_types WHERE component_id != ? ORDER BY (RAND() * (1+(SELECT COUNT(*) FROM images WHERE component_type = component_id))) DESC LIMIT 1;";
+    database.query(query, [lastDrawId], (err, result) => {
         if (err) {
             console.log(err);
         } else if (result.length >= 1) {
             async function combineData(r) {
                 let drawData = new Object();
                 drawData.type = r.file_prefix;
+                drawData.id = r.component_id;
                 drawData.componentText = r.draw_hint;
                 drawData.componentImg = await base64Helper.getBase64Img(r.component_hint_img);
                 drawData.labelText = "Please label the component somewhere";
