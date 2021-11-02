83f030feaf41644a154ee341f631012eceaade6b
PatrickZ1 <36849297+PatrickZ1@users.noreply.github.com>
2021-11-02 17:43:34 +0100

    new text

--- s2c-clientapp/src/Components/About.jsx
+++ s2c-clientapp/src/Components/About.jsx
@@ -3,15 +3,15 @@ import '../Styles/About.css';
 
 class About extends React.Component {
     render() {
-        const aboutText = "\
-        We - Tim Brunner, Sebastian Pfusterer, Patrick Ziegler - are a small team interested in AI-Development.\r\n\
-        This site is used to help us gather trainingsdata for our thesis: SketchToCircuit\r\n\
-        The goal of SketchToCircuit is to easily convert sketches of electronic circuits to a format understandable by simulation software and are easy to render.";
 
         return (
             <div className='about'>
                 <h1>About Us</h1>
-                <p>{aboutText}</p>
+                <p>
+                    We - Prof DI Rudolf Schamberger, Tim Brunner, Sebastian Pfusterer, Patrick Ziegler - are a small team from the higher technical college <a href="https://bulme.at/">HTL Bulme</a> interested in AI-Development.<br/>
+                    This site is used to help us gather trainingsdata for our thesis: SketchToCircuit<br/>
+                    The goal of SketchToCircuit is to easily convert sketches of electronic circuits to a format understandable by simulation and displaying software. We therefore hope to make the life of teachers and students easier by enabling them to use their sketches for further digital processing.
+                </p>
             </div>
         );
     }
--- s2c-clientapp/src/Components/StartButton.jsx
+++ s2c-clientapp/src/Components/StartButton.jsx
@@ -21,8 +21,10 @@ class StartButton extends React.Component {
         return (
             <div>
                 <p className='start-text'>
-                    You will either have to draw something in a given time or tell us if you see the correct image.<br/><br/>
-                    If you need help you can click the small image to get a bigger preview.
+                    You help us building new AI-technology<br/>
+                    If you continue, you will get a small task.
+                    You will either have to draw something in a given time or tell us if you see the correct image.<br/>
+                    If you need help you can click on the small image to get a bigger preview.
                 </p>
                 <button className='start-button' onClick={() => this.onBtnClick()}><span>Start!</span></button>
             </div>);
